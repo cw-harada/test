bdac568fcbabd20545e7f49220ba068e0f209589
cw-harada <harada@chatwork.com>
2017-06-02 17:56:08 +0900

    modfy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,57 +1,27 @@
-/*
- * grunt-dgeni
- * https://github.com/k-kinzal/grunt-dgeni
- *
- * Copyright (c) 2014 k-kinzal
- * Licensed under the MIT license.
- */
-module.exports = function (grunt) {
-  'use strict';
-  // grunt configuration
-  grunt.initConfig({
-	  d: {
-	    test1: {
-        message: 'run execute message1.',
-        type: 'confirm',
-        process: [
-          'cd node_modules',
-          'pwd',
-          'ls -la .',
-          'pause',
-          function name1(answer) {
-            console.log('answer:', answer);
-          }
-        ]
-      },
-      test2: {
-        message: 'run execute message2.',
-        type: 'confirm',
-        process: {
-          false: [
-            'cd tasks',
-            'pwd',
-            'ls -la .',
-          ]
-        }
-      },
-      test3: {
-        message: 'run execute message3.',
-        type: 'ok',
-        process: [
-          'fluentd -vv -c ~/.fluentd/fluent.conf &',
-          'pause',
-          'pwd',
-        ]
-      },
-      test4: {
-        message: 'run execute message4',
-        type: 'ok',
-        process: [
-          'wait echo 1 | grep 1',
-        ]
-      }
-	  },
-  });
+'use strict';
 
-  require('./tasks/d')(grunt);
+var shell = require('shelljs');
+
+module.exports = function (grunt) {
+    // grunt configuration
+    grunt.initConfig({
+        config: {},
+        d: {
+            test1: {
+                message: 'yml、jsのファイルが存在するか確認します',
+                type: 'ok',
+                process: [
+                    function () {
+                        var build_check_command = 'git diff master..develop --name-only "*.js" "*.yml"';
+                        var test = shell.exec(build_check_command);
+                        grunt.config('config.build', test);
+                        //console.log("bulidをする必要があります");
+                        console.log("bulidのファイル一覧\n" + grunt.config('config.build.output'));
+                    },
+                ]
+            },
+        },
+    });
+
+    require('./tasks/d')(grunt);
 };
